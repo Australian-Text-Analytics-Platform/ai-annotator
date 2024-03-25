e5c22003751570202463b3b11a0a99d1b15ab4ce
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-03-26 09:57:02 +1100

    feat: Add completions

---
+++ completions.py
@@ -0,0 +1,35 @@
+"""completions.py
+
+This depends heavily on the litellm package.
+
+Provide the functions to call the LLM.
+"""
+
+import asyncio
+
+from litellm import acompletion, ModelResponse
+
+
+def create_litellm_msg(msg: str) -> list[dict]:
+    return [{"content": msg, "role": "user"}]
+
+
+async def a_completion(model: str, msg: str) -> ModelResponse:
+    messages = create_litellm_msg(msg)
+    res = await acompletion(model=model, messages=messages)
+    return res
+
+
+async def a_batch_completions(model: str, msgs: list[str]) -> list[ModelResponse]:
+    tasks = [asyncio.create_task(a_completion(model=model, msg=msg)) for msg in msgs]
+    results: list[ModelResponse]
+    results = await asyncio.gather(*tasks)
+    return list(results)
+
+
+def batch_completions(
+    model: str,
+    msgs: list[str],
+) -> list[ModelResponse]:
+    results = asyncio.run(a_batch_completions(model, msgs))
+    return results
