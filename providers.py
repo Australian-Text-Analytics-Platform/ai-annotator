9d9db04e96e23d08fb5035d96de07abf308f4967
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-03-26 08:15:17 +1100

    feat: Add providers

---
+++ providers.py
@@ -0,0 +1,59 @@
+"""classifier.py
+
+Provide the core functionalities of the generic LLM classifier.
+
+1. select provider
+2. provide the list of models that I can select from for this provider.
+3. given the model, prompt and list of texts, run the batch classification. (stream=False version)
+"""
+
+from enum import Enum
+from typing import Callable
+
+from pydantic import BaseModel, Field
+
+from open_ai import list_models as open_ai_list_models
+from azure_open_ai import list_models as azure_open_ai_list_models
+
+_list_models_registry: dict[str, Callable[..., list[str]]] = {
+    "open_ai": open_ai_list_models,
+    "azure_open_ai": azure_open_ai_list_models,
+}
+
+
+class LLMProviderContext(BaseModel):
+    name: str = Field(frozen=True)
+    tooltip: str = Field(frozen=True)
+    models: list[str] = Field(frozen=True)
+
+
+def create_llm_context(name: str, tooltip: str, key: str) -> LLMProviderContext:
+    try:
+        models: list[str] = _list_models_registry[key]()
+    except KeyError as ke:
+        raise KeyError(f"{key} is not valid key for a provider.") from ke
+
+    return LLMProviderContext(
+        name=name,
+        tooltip=tooltip,
+        models=models,
+    )
+
+
+class LLMProvider(Enum):
+    OPENAI: LLMProviderContext = create_llm_context(
+        name="OpenAI",
+        tooltip="OpenAI the company.",
+        key="open_ai",
+    )
+    AZURE_OPENAI: LLMProviderContext = create_llm_context(
+        name="Azure OpenAI",
+        tooltip="Same as OpenAI but hosted in the Azure cloud provider.",
+        key="azure_open_ai",
+    )
+    # note: extend here for more providers.
+
+
+def list_llm_providers_as_str() -> list[str]:
+    """List all available LLM Providers's name."""
+    return list(map(lambda llmp: llmp.value.name, (llmp for llmp in LLMProvider)))
