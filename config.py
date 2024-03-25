da46dd581c6b940a037d0b3c00e7b0fcc25bd6fa
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-03-26 09:56:04 +1100

    feat(config): API key not instantiated at app init

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
-__all__ = ["settings"]
-
 
 class APISettings(BaseSettings):
-    OPENAI_API_KEY: str = Field(min_length=10, max_length=10)
-
-
-settings = APISettings()
+    OPENAI_API_KEY: str = Field(min_length=51, max_length=51)
