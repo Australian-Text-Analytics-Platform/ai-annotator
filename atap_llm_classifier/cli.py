0b9f273d44bbb25e414b1a47c534f09398478a9f
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-04-11 12:02:22 +1000

    feat(cli): rm google-fire, use typer

--- atap_llm_classifier/cli.py
+++ atap_llm_classifier/cli.py
@@ -1,39 +1,23 @@
-from enum import Enum
-from typing import Callable
+import functools
+import typer
 
-try:
-    import fire
-except ImportError as e:
-    raise ImportError(
-        "Please install with 'cli' extras. e.g. pip install 'atap-llm-classifier[cli]'"
-    )
+from atap_llm_classifier.litellm_utils import pretty_print_model_list
+
+# subcommand: litellm
+
+cli_litellm: typer.Typer = typer.Typer()
 
 
-class Command(Enum):
-    LITELLM_LIST_MODELS: str = "litellm.list_models"
+@functools.wraps(pretty_print_model_list)
+@cli_litellm.command(name="list-models")
+def list_models(*args, **kwargs):
+    return pretty_print_model_list(*args, **kwargs)
+
+
+# main cli
+cli: typer.Typer = typer.Typer()
+cli.add_typer(cli_litellm, name="litellm")
 
 
 def main():
-    from atap_llm_classifier.litellm_utils import pretty_print_model_list
-
-    fire.Fire(
-        {
-            "litellm": {
-                "list_models": pretty_print_model_list,
-            },
-        }
-    )
-
-
-def _get_fn(cmd: Command) -> Callable:
-    match cmd:
-        case Command.LITELLM_LIST_MODELS:
-            from atap_llm_classifier.litellm_utils import pretty_print_model_list
-
-            return pretty_print_model_list
-        case _:
-            raise NotImplementedError()
-
-
-if __name__ == "__main__":
-    main()
+    cli()
