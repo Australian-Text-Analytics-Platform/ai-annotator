1f9f7a8d19d4cb36460192dd660e2a11cffabd4c
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-05-08 11:00:28 +1000

    test(assets): rename fn for clarity

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -2,13 +2,13 @@ from atap_llm_classifier.techniques.techniques import Technique
 from atap_llm_classifier.modifiers.modifiers import Modifier
 
 
-def test_modifier_assets():
+def test_modifier_assets_all_exist_and_valid():
     for modifier in Modifier:
         ctx = modifier.get_context()
     # pass if no exceptions raised.
 
 
-def test_technique_assets():
+def test_technique_assets_all_exist_and_valid():
     for technique in Technique:
         ctx = technique.get_context()
     # pass if no exceptions raised.
