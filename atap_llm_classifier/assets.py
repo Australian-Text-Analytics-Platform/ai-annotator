6e3aecc06282524f54eda16eb4cf2b1bbf074281
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-05-08 10:38:07 +1000

    feat: Add assets

---
+++ atap_llm_classifier/assets.py
@@ -0,0 +1,83 @@
+"""utils.py"""
+
+from functools import lru_cache
+from io import IOBase
+from pathlib import Path
+from typing import TypeVar, Type
+from enum import Enum
+
+import yaml
+from pydantic import BaseModel
+
+from .techniques.techniques import TechniqueContext
+from .modifiers.modifiers import ModifierContext
+
+
+class Asset(Enum):
+    TECHNIQUES: str = "techniques"
+    MODIFIERS: str = "modifiers"
+
+    def get_path(self) -> Path:
+        match self:
+            case Asset.TECHNIQUES:
+                return asset_dir / "techniques.yml"
+            case Asset.MODIFIERS:
+                return asset_dir / "modifiers.yml"
+
+
+@lru_cache(maxsize=len(Asset))
+def load_asset(asset: Asset) -> dict:
+    with open(asset.get_path(), "r", encoding="utf-8") as h:
+        return yaml.safe_load(h)
+
+
+@lru_cache()
+def get_modifiers_asset(
+    key: str,
+    **overrides: dict,
+) -> ModifierContext:
+    data: dict = load_asset(Asset.MODIFIERS)
+    datum = data[key]
+    for k, v in overrides.items():
+        datum[k] = v
+    return ModifierContext(**datum)
+
+
+@lru_cache()
+def get_techniques_asset(
+    key: str,
+    **overrides: dict,
+) -> TechniqueContext:
+    data: dict = load_asset(Asset.TECHNIQUES)
+    datum = data[key]
+    for k, v in overrides.items():
+        datum[k] = v
+    return TechniqueContext(**datum)
+
+
+# Below unused: kept for now.
+
+TBaseModel = TypeVar("TBaseModel", bound=BaseModel)
+
+
+def load_model_from_yaml(
+    path_or_stream: str | IOBase,
+    model_cls: Type[TBaseModel],
+) -> TBaseModel:
+    model_data: dict
+    if isinstance(path_or_stream, str):
+        path = Path(path_or_stream)
+        if not path.is_file():
+            raise FileNotFoundError(
+                f"{path_or_stream} is not a file. "
+                f"Assumes path_or_stream is path when given str."
+            )
+        with open(path, "r", encoding="utf-8") as h:
+            model_data = yaml.safe_load(h)
+    else:
+        model_data = yaml.safe_load(path_or_stream)
+    return model_cls.model_validate(model_data)
+
+
+asset_dir: Path = Path(__file__).parent.parent / "assets"
+assert asset_dir.exists(), f"Asset directory: {asset_dir.absolute()} not found."
