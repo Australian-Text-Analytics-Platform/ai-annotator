5ce8fa9a4e8ce13b1c84c6e8ddb8b92a111df64b
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-05-28 10:37:41 +1000

    feat(view): pipe_cls - Add  btns - wip

--- atap_llm_classifier/views/parts/pipeline_classifications.py
+++ atap_llm_classifier/views/parts/pipeline_classifications.py
@@ -8,6 +8,7 @@ class PipelineClassifications(Viewer):
     def __init__(self, corpus: Corpus, **params):
         super().__init__(**params)
         self.corpus = corpus
+        self.corpus_rx = pn.rx(corpus)
 
         df = corpus.docs().to_frame(name="document")
 
@@ -19,11 +20,36 @@ class PipelineClassifications(Viewer):
             disabled=True,
         )
 
-        # todo: classify one
-        btn = pn.widgets.Button(name="Run")
+        classify_one_btn = pn.widgets.Button(
+            name="Classify One",
+            width=120,
+            margin=(15, 5),
+        )
+        one_idx_inp = pn.widgets.IntInput(
+            name="doc index",
+            value=0,
+            start=0,
+            end=len(self.corpus),
+            width=80,
+        )
+        one_idx_doc = pn.pane.Markdown(self.corpus_rx[one_idx_inp.rx()], margin=(10, 20))
+        classify_all_btn = pn.widgets.Button(
+            name="Classify All",
+            width=classify_one_btn.width,
+            margin=(0, classify_one_btn.margin[1]),
+        )
+        all_progress_bar = pn.widgets.Tqdm()
         self.layout = pn.Column(
-            pn.Row(btn),
             self.df_widget,
+            pn.Row(
+                classify_one_btn,
+                one_idx_inp,
+                one_idx_doc,
+            ),
+            pn.Row(
+                classify_all_btn,
+                all_progress_bar,
+            ),
         )
 
     def __panel__(self) -> Viewable:
