e3cc24ff9188a693f70cd5483b6d5b77457a115b
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-06-06 14:28:24 +1000

    test: Add test pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,33 @@
+from atap_corpus import Corpus
+
+from atap_llm_classifier import config, pipeline, models
+from atap_llm_classifier.modifiers import Modifier
+from atap_llm_classifier.providers import LLMProvider
+from atap_llm_classifier.techniques import Technique, schemas
+
+
+def test_batch():
+    config.mock = True
+    config.mock_tokens_rate_limit = None
+    config.mock_requests_rate_limit = None
+
+    corpus: Corpus = Corpus([f"Test document {i}" for i in range(10)])
+
+    res: pipeline.BatchResults = pipeline.batch(
+        corpus=corpus,
+        provider=LLMProvider.OPENAI,
+        model="gpt-3.5-turbo",
+        api_key="",
+        llm_config=models.LLMConfig(),
+        technique=Technique.ZERO_SHOT,
+        user_schema=schemas.ZeroShotUserSchema(
+            classes=[
+                schemas.ZeroShotClass(
+                    name="class name", description="class description"
+                )
+            ]
+        ),
+        modifier=Modifier.NO_MODIFIER,
+        on_result_callback=None,
+    )
+    assert len(res.successes) == len(corpus)
